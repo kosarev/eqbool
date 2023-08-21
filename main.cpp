14978be1d11ffbfe6a120942eeb1baa2817c0a1f
Ivan Kosarev <mail@ivankosarev.com>
2023-08-21 21:45:35 +0100

    Have a class storing the test context.

--- main.cpp
+++ main.cpp
@@ -13,40 +13,49 @@
 
 #include "eqbool.h"
 
+namespace {
+
 [[noreturn]] static void fatal(const char *msg) {
     std::cerr << "error: " << msg << std::endl;
     std::exit(EXIT_FAILURE);
 }
 
-static void process_new_term(unsigned term) {
-    std::cout << "new term: " << term << "\n";
-}
-
-static void process_test_line(const std::string &line) {
-    std::istringstream s(line);
-    char op;
-    if(!(s >> op))
-        fatal("operator missed");
-    if(op == '.') {
-        unsigned term;
-        if(!(s >> term))
-            fatal("term missed");
-        process_new_term(term);
-        return;
+class test_context {
+private:
+    void process_new_term(unsigned term) {
+        std::cout << "new term: " << term << "\n";
     }
 
-    fatal("unknown operator");
-}
+    void process_test_line(const std::string &line) {
+        std::istringstream s(line);
+        char op;
+        if(!(s >> op))
+            fatal("operator missed");
+        if(op == '.') {
+            unsigned term;
+            if(!(s >> term))
+                fatal("term missed");
+            process_new_term(term);
+            return;
+        }
 
-static void process_test_lines() {
-    std::string line;
-    while(std::getline(std::cin, line))
-        process_test_line(line);
+        fatal("unknown operator");
+    }
 
-    if(!std::cin.eof())
-        fatal("cannot read input");
-}
+public:
+    void process_test_lines() {
+        std::string line;
+        while(std::getline(std::cin, line))
+            process_test_line(line);
+
+        if(!std::cin.eof())
+            fatal("cannot read input");
+    }
+};
+
+}  // anonymous namespace
 
 int main() {
-    process_test_lines();
+    test_context c;
+    c.process_test_lines();
 }
