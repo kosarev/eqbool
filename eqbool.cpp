124c345d5c92be82d86c21ec15d22c4cf5f55a72
Ivan Kosarev <mail@ivankosarev.com>
2023-08-27 22:00:08 +0100

    Simplify both EQ A 1 and EQ 1 A to A.

--- eqbool.cpp
+++ eqbool.cpp
@@ -97,6 +97,8 @@ eqbool eqbool_context::get_eq(eqbool a, eqbool b) {
 
     if(a.is_true())
         return b;
+    if(b.is_true())
+        return a;
     if(b.is_false())
         return ~a;
     if(a.is_const() && b.is_const())
