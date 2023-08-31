112319da1acef4a1af93b98adfdb3d2f41d14a37
Ivan Kosarev <mail@ivankosarev.com>
2023-08-31 18:38:28 +0100

    Fix counting clauses.

--- eqbool.cpp
+++ eqbool.cpp
@@ -148,6 +148,7 @@ bool eqbool_context::is_unsat(eqbool e) {
 
     solver->add(skip_not(e));
     solver->add(0);
+    ++stats.num_clauses;
 
     std::vector<eqbool> worklist({e});
     std::unordered_set<const node_def*> visited;
@@ -168,6 +169,7 @@ bool eqbool_context::is_unsat(eqbool e) {
                 assert(n.is_false());
                 solver->add(-r_lit);
                 solver->add(0);
+                ++stats.num_clauses;
             }
             continue;
         case node_kind::or_node: {
