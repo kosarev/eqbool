bd11ffb9e2a86d79d335b1bcc703f932ec2f6b63
Ivan Kosarev <mail@ivankosarev.com>
2025-06-15 15:18:44 +0100

    [NFC] Simplify ifelse().

--- eqbool.cpp
+++ eqbool.cpp
@@ -371,35 +371,9 @@ eqbool eqbool_context::ifelse(eqbool i, eqbool t, eqbool e) {
         if(t == e)
             return t;
 
-        if(t == ~e) {
-            if(t < i) {
-                std::tie(i, t, e) = std::make_tuple(t, i, ~i);
-                continue;
-            }
-
-            bool inv = false;
-            if(i.is_inversion()) {
-                i = ~i;
-                inv = !inv;
-            }
-            if(t.is_inversion()) {
-                t = ~t;
-                inv = !inv;
-            }
-
-            // We only consider the case when t contains i, because we
-            // know i was created before t (i < t).
-            const node_def &t_def = t.get_def();
-            if(t_def.kind == node_kind::eq) {
-                if(t_def.args[0] == i)
-                    return inv ? ~t_def.args[1] : t_def.args[1];
-                if(t_def.args[1] == i)
-                    return inv ? ~t_def.args[0] : t_def.args[0];
-            }
-
-            node_def def(node_kind::eq, {i, t}, *this);
-            eqbool r = add_def(def);
-            return inv ? ~r : r;
+        if(t == ~e && t < i) {
+            std::tie(i, t, e) = std::make_tuple(t, i, ~i);
+            continue;
         }
 
         break;
@@ -408,11 +382,30 @@ eqbool eqbool_context::ifelse(eqbool i, eqbool t, eqbool e) {
     if(i.is_inversion())
         std::tie(i, t, e) = std::make_tuple(~i, e, t);
 
-    bool inv = false;
-    if(t.is_inversion() && e.is_inversion()) {
-        std::tie(t, e) = std::make_tuple(~t, ~e);
-        inv = !inv;
+    if(t == ~e) {
+        assert(!i.is_inversion());
+        bool inv = t.is_inversion();
+        if(inv)
+            t = ~t;
+
+        // We only consider the case when t contains i, because we
+        // know i was created before t (i < t).
+        const node_def &t_def = t.get_def();
+        if(t_def.kind == node_kind::eq) {
+            if(t_def.args[0] == i)
+                return inv ? ~t_def.args[1] : t_def.args[1];
+            if(t_def.args[1] == i)
+                return inv ? ~t_def.args[0] : t_def.args[0];
+        }
+
+        node_def def(node_kind::eq, {i, t}, *this);
+        eqbool r = add_def(def);
+        return inv ? ~r : r;
     }
+
+    bool inv = t.is_inversion() && e.is_inversion();
+    if(inv)
+        std::tie(t, e) = std::make_tuple(~t, ~e);
 
     node_def def(node_kind::ifelse, {i, t, e}, *this);
     eqbool r = add_def(def);
