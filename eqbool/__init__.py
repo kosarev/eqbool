4c527faf0f6cd7862478050fa5d0ba09366b70e7
Ivan Kosarev <mail@ivankosarev.com>
2025-07-13 22:48:56 +0100

    Fix mypy complaints.

--- eqbool/__init__.py
+++ eqbool/__init__.py
@@ -13,13 +13,15 @@ from ._eqbool import _Bool
 from ._eqbool import _Context
 from ._main import main
 
+import typing
+
 
 class Bool(_Bool):
-    def __init__(self):
-        self.context = None
+    def __init__(self) -> None:
+        self.context: None | Context = None
 
     @classmethod
-    def _make(cls, __c, __v):
+    def _make(cls, __c: 'Context', __v: _Bool) -> 'Bool':
         assert isinstance(__c, Context)
         assert type(__v) is _Bool
         b = cls()
@@ -28,57 +30,61 @@ class Bool(_Bool):
         return b
 
     @property
-    def void(self):
+    def void(self) -> bool:
         return self.context is None
 
     @property
-    def id(self):
+    def id(self) -> int:
         assert not self.void
         return self._get_id()
 
-    def __str__(self):
+    def __str__(self) -> str:
         assert not self.void
         return self._print()
 
-    def __invert__(self):
+    def __invert__(self) -> 'Bool':
+        assert self.context is not None
         return type(self)._make(self.context, self._invert())
 
-    def __or__(self, other):
+    def __or__(self, other: 'Bool') -> 'Bool':
+        assert self.context is not None
         return self.context.get_or(self, other)
 
-    def __and__(self, other):
+    def __and__(self, other: 'Bool') -> 'Bool':
+        assert self.context is not None
         return self.context.get_and(self, other)
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        assert isinstance(other, Bool)
         assert other.context is self.context
         return self.id == other.id
 
 
 class Context(_Context):
-    def __init__(self, bool_type=Bool):
+    def __init__(self, bool_type: typing.Type[Bool] = Bool) -> None:
         self.__t = bool_type
 
-    def _make(self, v):
+    def _make(self, v: _Bool) -> Bool:
         return self.__t._make(self, v)
 
-    def get(self, v):
+    def get(self, v: bool | str | int | typing.Tuple[typing.Any, ...]) -> Bool:
         return self._make(self._get(v))
 
     @property
-    def false(self):
+    def false(self) -> Bool:
         return self.get(False)
 
     @property
-    def true(self):
+    def true(self) -> Bool:
         return self.get(True)
 
-    def get_or(self, *args):
+    def get_or(self, *args: Bool) -> Bool:
         assert all(a.context is self for a in args)
         return self._make(self._get_or(*args))
 
-    def get_and(self, *args):
+    def get_and(self, *args: Bool) -> Bool:
         return ~self.get_or(*(~a for a in args))
 
-    def ifelse(self, i, t, e):
+    def ifelse(self, i: Bool, t: Bool, e: Bool) -> Bool:
         assert all(a.context is self for a in (i, t, e))
         return self._make(self._ifelse(i, t, e))
--- eqbool/_eqbool.pyi
+++ eqbool/_eqbool.pyi
@@ -2,8 +2,25 @@
 import typing
 
 class _Bool:
-    pass
+    def _set(self, v: _Bool) -> None:
+        ...
+
+    def _get_id(self) -> int:
+        ...
+
+    def _invert(self) -> _Bool:
+        ...
+
+    def _print(self) -> str:
+        ...
 
 
 class _Context:
-    pass
+    def _get(self, v: str | int | typing.Tuple[typing.Any, ...]) -> _Bool:
+        ...
+
+    def _get_or(self, *args: _Bool) -> _Bool:
+        ...
+
+    def _ifelse(self, i: _Bool, t: _Bool, e: _Bool) -> _Bool:
+        ...
